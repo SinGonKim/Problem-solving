921e88cce0784884058943db8358337ba9a1de55
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-04-12 13:25:27 +0900

    Title: XOR 결과 최대 만들기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250412/XOR 결과 최대 만들기/max-of-xor.py
+++ 250412/XOR 결과 최대 만들기/max-of-xor.py
@@ -1,67 +1,19 @@
-from collections import defaultdict
-def get_xor(arr):
-    result = 0
-    for i in range(len(arr)):
-        if i == 0:
-            result = nums[arr[i]]
-        else:
-            result = nums[arr[i]] ^ result
+from itertools import combinations
 
-    return result
-
-
-def choose(idx):
-    global max_xor
-    if idx == M + 1:
-
-        # xor 계산
-        xor = get_xor(arr)
-
-        if xor > max_xor:
-            max_xor = xor
-
-        return
-
-
-    for i in range(1, N + 1):
-
-        if not visited[i]:
-
-            # memoization
-            temp = arr[:]
-            temp.append(i)
-            key = "".join(map(str,temp))
-
-            if memo[key] == 1:
-                continue
-            memo[key] = 1
-
-            visited[i] = True
-
-            arr.append(i)
-            choose(idx + 1)
-            arr.pop()
-
-            visited[i] = False
-
+def get_max_xor(nums, M):
+    max_xor = 0
+    for comb in combinations(nums, M):
+        xor = 0
+        for num in comb:
+            xor ^= num
+        max_xor = max(max_xor, xor)
+    return max_xor
 
 if __name__ == "__main__":
     N, M = map(int, input().split())
-
-
-    nums = [0] + sorted(list(map(int, input().split())))
+    nums = list(map(int, input().split()))
 
     if M == 1:
         print(max(nums))
-
     else:
-        
-        max_xor = 0
-
-        arr = []
-        memo = defaultdict(int)
-
-        visited = [True] + [False] * N
-
-        choose(1)
-        print(max_xor)
+        print(get_max_xor(nums, M))
