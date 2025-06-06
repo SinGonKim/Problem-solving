591cb4b3706efb92974a3029d0dc0db323aa8a6c
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-06-06 19:19:27 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 406ms, Memory: 59MB, Status: Passed - Codetree

--- 250606/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 250606/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -4,13 +4,17 @@ def is_range(x,y):
 def simulate(x,y,cnt):
     dxs = [-1,1,0,0]
     dys = [0,0,-1,1]
+    dp[x][y] = max(dp[x][y], cnt)
 
     for dx, dy in zip(dxs, dys):
         nx = x + dx; ny = y + dy
         if is_range(nx,ny) and grid[nx][ny] > grid[x][y]:
-            simulate(nx,ny,cnt+1)
-    dp[x][y] = max(dp[x][y], cnt)
-    return
+            if dp[nx][ny] == 0:
+                result =  simulate(nx,ny,1)
+            else:
+                result = dp[nx][ny]
+            dp[x][y] = max(dp[x][y], cnt+result)
+    return dp[x][y]
 
 
 if __name__ == "__main__":
@@ -18,17 +22,13 @@ if __name__ == "__main__":
     grid = [list(map(int, input().split())) for _ in range(n)]
 
     # Please write your code here.
-    dp = [[1 for _ in range(n)] for _ in range(n)]
-    visited = [[-1 for _ in range(n)] for _ in range(n)]
+    dp = [[0 for _ in range(n)] for _ in range(n)]
 
     for i in range(n):
         for j in range(n):
-            if visited[i][j] == -1:
+            if dp[i][j] == 0:
                 simulate(i,j,1)
-            else:
-                row = visited[i][j]//n
-                col = visited[i][j]%n
-                dp[i][j] = dp[i][j] + dp[row][col]
+    
     target = -1
     for i in range(n):
         for j in range(n):
