68bf3b7d7cb46d945eda343bfcaa0e4d01ae10d8
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-06-27 02:06:02 +0900

    Title: 최대 증가 부분 수열, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 250627/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 250627/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -3,7 +3,7 @@ m = list(map(int, input().split()))
 
 # Please write your code here.
 import bisect
-dp = [1 for _ in range(n)]
+
 stack = []
 stack.append(m[0])
 
@@ -11,10 +11,10 @@ for i in range(1,n):
     idx = bisect.bisect_left(stack,m[i])
     if idx == len(stack):
         stack.append(m[i])
-        dp[i] = dp[idx-1]+1
+  
     else:
         stack[idx] = m[i]
-        dp[i] = dp[idx]
+     
  
-print(max(dp))
+print(len(stack))
 
