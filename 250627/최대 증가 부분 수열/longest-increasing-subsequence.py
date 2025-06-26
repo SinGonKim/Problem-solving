df5befdf0fd2a0d20f390377b698b8d0b2ed67c0
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-06-27 01:53:18 +0900

    Title: 최대 증가 부분 수열, Time: 55ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250627/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 250627/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -2,11 +2,19 @@ n = int(input())
 m = list(map(int, input().split()))
 
 # Please write your code here.
+import bisect
 dp = [0 for _ in range(n)]
+stack = []
+stack.append(m[0])
 dp[0] = 1
 for i in range(1,n):
-    for j in range(i):
-        if m[i] > m[j]:
-            dp[i] = max(dp[i], dp[j]+1)
+    idx = bisect.bisect_left(stack,m[i])
+    if idx == len(stack):
+        stack.append(m[i])
+        dp[i] = dp[idx-1]+1
+    else:
+        stack[idx] = m[i]
+        dp[i] = dp[idx]
+ 
 print(max(dp))
 
