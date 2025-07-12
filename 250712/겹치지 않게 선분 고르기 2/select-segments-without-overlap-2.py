0f493938cc208d27d4165ecc85730f9870fbaedf
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-07-12 15:30:15 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 70ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250712/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 250712/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -2,21 +2,21 @@ import sys
 MIN_INT = -sys.maxsize -1
 n = int(input())
 a = [tuple(map(int, input().split())) for _ in range(n)]
-a.sort(key=lambda x: (x[1],x[0]), reverse=True)
+# a.sort(key=lambda x: (x[1],x[0]), reverse=True)
+a.sort(key=lambda x: x[1])
 
 # Please write your code here.
-dp = [1 for _ in range(n)]
+
 
 def solution():
-    n = len(a)
+    cnt = 0
+    last_end = -1
 
-    for i in range(1, n):
-        my_x, my_y = a[i]
+    for s, e in a:
+        if s >= last_end:
+            cnt += 1
+            last_end = e
+    return cnt
 
-        for j in range(0,i):
-            before_x, before_y = a[j]
 
-            if my_x == before_x or my_y == before_y:continue
-            dp[i] = max(dp[j]+1, dp[i])
-    return max(dp)
 print(solution())
