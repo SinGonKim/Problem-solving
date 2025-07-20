abb82ceb263e2825819a6e269263263da5b6ea70
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-07-20 20:14:06 +0900

    Title: 고대 보물 지도의 비밀, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250720/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
+++ 250720/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -12,13 +12,13 @@ for i in range(n+1):
         dp[i][1] = numbers[i]
 max_result = max(dp[0])
 
-for i in range(1,n+1):
+for i in range(1, n+1):
     for j in range(k+1):
         if numbers[i] >= 0:
-            if dp[i-1][j] == -INF:
-                dp[i][j] = max(dp[i][j], numbers[i])
-            else:
-                dp[i][j] = max(dp[i-1][j] + numbers[i], numbers[i])
+            if dp[i-1][j] != -INF:
+                dp[i][j] = dp[i-1][j] + numbers[i]
+            # i번째 숫자에서 새로운 구간을 시작하는 경우
+            dp[i][j] = max(dp[i][j], numbers[i])
         else:
             if j>=1:
                 if dp[i-1][j-1] == -INF:
