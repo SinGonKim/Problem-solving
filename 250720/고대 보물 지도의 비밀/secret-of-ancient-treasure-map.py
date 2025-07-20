66035e48f7ecc3b2ae4298da4f7f93f5be73a45d
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-07-20 20:20:35 +0900

    Title: 고대 보물 지도의 비밀, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250720/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
+++ 250720/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -5,26 +5,28 @@ INF = sys.maxsize
 # Please write your code here.
 dp = [[-INF for _ in range(k+1)] for _ in range(n+1)]
 
-for i in range(n+1):
-    if numbers[i] >= 0:
-        dp[i][0] = numbers[i]
-    else:
-        dp[i][1] = numbers[i]
-max_result = max(dp[0])
+# 초기화: 첫 번째 숫자(numbers[1])
+if numbers[1] >= 0:
+    dp[1][0] = numbers[1]
+else:
+    dp[1][1] = numbers[1]  # K ≥ 1이므로 안전
+max_result = max(dp[1])
 
-for i in range(1, n+1):
+for i in range(2, n+1):
     for j in range(k+1):
         if numbers[i] >= 0:
             if dp[i-1][j] != -INF:
                 dp[i][j] = dp[i-1][j] + numbers[i]
             # i번째 숫자에서 새로운 구간을 시작하는 경우
-            dp[i][j] = max(dp[i][j], numbers[i])
+            if j == 0:
+                dp[i][j] = max(dp[i][j], numbers[i])
         else:
             if j>=1:
-                if dp[i-1][j-1] == -INF:
-                    dp[i][j] = max(dp[i][j], numbers[i])
-                else:
-                    dp[i][j] = max(dp[i-1][j-1]+numbers[i], numbers[i])
+                if dp[i-1][j-1] != -INF:
+                    dp[i][j] = dp[i-1][j-1]+numbers[i]
+
+                dp[i][j] = max(dp[i][j], numbers[i])
+
     max_result = max(max_result, max(dp[i]))
 
 print(max_result)
