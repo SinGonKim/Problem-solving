1c393e71fb905ecc16dc8b93db6a80aad4f591d5
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-07-20 20:02:51 +0900

    Title: 고대 보물 지도의 비밀, Time: 120ms, Memory: 25MB, Status: Wrong Answer - Codetree

---
+++ 250720/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -0,0 +1,30 @@
+n, k = map(int, input().split())
+numbers = [0] + list(map(int, input().split()))
+import sys
+INF = sys.maxsize
+# Please write your code here.
+dp = [[-INF for _ in range(k+1)] for _ in range(n+1)]
+
+for i in range(n+1):
+    if numbers[i]:
+        dp[i][0] = numbers[i]
+    else:
+        dp[i][1] = numbers[i]
+max_result = max(dp[0])
+
+for i in range(1,n+1):
+    if numbers[i] >= 0:
+        for j in range(k):
+            if dp[i-1][j] == -INF:
+                dp[i][j] = max(numbers[i], dp[i][j])
+            else:
+                dp[i][k] = max(dp[i-1][k] + numbers[i], numbers[i])
+    else:
+        for j in range(1,k+1):
+            if dp[i-1][j-1] == -INF:
+                dp[i][k] = max(dp[i][k], numbers[i])
+            else:
+                dp[i][j] = max(dp[i-1][j-1]+numbers[i], numbers[i])
+    max_result = max(max_result, max(dp[i]))
+
+print(max_result)
