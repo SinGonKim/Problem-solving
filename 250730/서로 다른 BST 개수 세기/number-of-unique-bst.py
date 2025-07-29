057be59a1d485bee03d12a46dbb9068caa70e620
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-07-30 00:57:21 +0900

    Title: 서로 다른 BST 개수 세기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250730/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+# Please write your code here.
+dp = [0 for _ in range(21)]
+
+
+def test(n):
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 5
+
+    if dp[n] != 0:
+        return dp[n]
+    
+    if n <= 1:
+        return 1
+    
+    temp = 0
+    for i in  range(n):
+        temp += test(i) *  test(n-i-1)
+    dp[n] = temp
+    return dp[n]
+print(test(n))
