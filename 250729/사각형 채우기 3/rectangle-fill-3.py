4be3eea3bdbd83cfb8d3b294bb7dc15ad5f4543f
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-07-29 20:11:29 +0900

    Title: 사각형 채우기 3, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250729/사각형 채우기 3/rectangle-fill-3.py
+++ 250729/사각형 채우기 3/rectangle-fill-3.py
@@ -2,9 +2,15 @@ n = int(input())
 mod = 1000000007
 # Please write your code here.
 dp = [0 for _ in range(1001)]
+dphalf = [0 for _ in range(1001)]
 dp[0] = 1
 dp[1] = 2
 dp[2] = 7
+
+dphalf[1] = 1
+dphalf[2] = 3
+
 for i in range(3,n+1):
-    dp[i] = (2*dp[i-1] + 3*dp[i-2] + 2*dp[i-3])%mod
+    dp[i] = (2*dp[i-1] + dp[i-2] +dphalf[i-1]*2)%mod
+    dphalf[i] = (dp[i-1] + dphalf[i-1])%mod
 print(dp[n])
