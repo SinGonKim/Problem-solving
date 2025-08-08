b1519bf217f55fe085aefbb88eb2afd859c65de1
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-08-08 10:12:40 +0900

    Title: 뱀은 사과를 좋아해, Time: 54ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250808/뱀은 사과를 좋아해/snake-loves-apples.py
+++ 250808/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -5,108 +5,57 @@ apples = [tuple(map(lambda x: int(x)-1, input().split())) for _ in range(M)]
 movement = [tuple(map(lambda x: int(x) if x.isdecimal() else x, input().split())) for _ in range(K)]
 
 # 뱀의 모든 좌표를 기억해두는 배열
-snake = deque()
-# 사과 위치를 표시하는 배열
-apple = [[False for _ in range(N)] for __ in range(N)]
-# 뱀 위치를 표시하는 배열
-board = [[False for _ in range(N)] for __ in range(N)]
+snake = deque([(0, 0)])
+# 사과 위치를 표시하는 set (O(1) 조회)
+apple_set = {(apple_x, apple_y) for apple_x, apple_y in apples}
+# 뱀 위치를 표시하는 set (O(1) 조회 및 추가/삭제)
+snake_set = {(0, 0)}
 
-# 변경된 뱀의 위치를 board에 반영하는 함수
-def apply_snake_loc():
-    global board
-    for i in range(N):
-        for j in range(N):
-            board[i][j] = False
+# 방향 매핑을 딕셔너리로 정의 (switch-case 대신)
+directions = {
+    'L': (0, -1),
+    'R': (0, 1),
+    'U': (-1, 0),
+    'D': (1, 0)
+}
 
-    for loc_x, loc_y in snake:
-        if board[loc_x][loc_y]:
-            return True
-        board[loc_x][loc_y] = True
-    return False
-
-# 초기 게임 세팅 함수
-def setUp():
-    snake.append((0, 0))
-    apply_snake_loc()
-
-    for apple_x, apple_y in apples:
-        apple[apple_x][apple_y] = True
-
-# 정해진 방향으로 뱀을 움직이는 함수
 def move(direction):
-    X, Y, HEAD = 0, 1, 0
-    x, y = snake[HEAD]
-
-    if direction == "L":
-        nx, ny = x, y-1
-        if nx < 0 or nx >= N or ny < 0 or ny >= N:
-            return True
-        # 사과를 먹었다면...
-        if apple[nx][ny]:
-            apple[nx][ny] = False
-            snake.appendleft((nx, ny))
-        # 사과를 못먹었다면...
-        else:
-            snake.appendleft((nx, ny))
-            snake.pop()
-    elif direction == "R":
-        nx, ny = x, y+1
-        if nx < 0 or nx >= N or ny < 0 or ny >= N:
-            return True
-        # 사과를 먹었다면...
-        if apple[nx][ny]:
-            apple[nx][ny] = False
-            snake.appendleft((nx, ny))
-        # 사과를 못먹었다면...
-        else:
-            snake.appendleft((nx, ny))
-            snake.pop()
-    elif direction == "U":
-        nx, ny = x-1, y
-        if nx < 0 or nx >= N or ny < 0 or ny >= N:
-            return True
-        # 사과를 먹었다면...
-        if apple[nx][ny]:
-            apple[nx][ny] = False
-            snake.appendleft((nx, ny))
-        # 사과를 못먹었다면...
-        else:
-            snake.appendleft((nx, ny))
-            snake.pop()
-    else:
-        nx, ny = x+1, y
-        if nx < 0 or nx >= N or ny < 0 or ny >= N:
-            return True
-        # 사과를 먹었다면...
-        if apple[nx][ny]:
-            apple[nx][ny] = False
-            snake.appendleft((nx, ny))
-        # 사과를 못먹었다면...
-        else:
-            snake.appendleft((nx, ny))
-            snake.pop()
-
-    ended = apply_snake_loc()
-    if ended:
+    head_x, head_y = snake[0]  # deque의 첫 번째 원소가 머리
+    dx, dy = directions[direction]
+    nx, ny = head_x + dx, head_y + dy
+    
+    # 경계 체크
+    if nx < 0 or nx >= N or ny < 0 or ny >= N:
         return True
+    
+    # 자기 몸과 충돌 체크
+    if (nx, ny) in snake_set:
+        return True
+    
+    # 새로운 머리 위치 추가
+    snake.appendleft((nx, ny))
+    snake_set.add((nx, ny))
+    
+    # 사과를 먹었는지 확인
+    if (nx, ny) in apple_set:
+        apple_set.remove((nx, ny))
+    else:
+        # 사과를 못 먹었다면 꼬리 제거
+        tail = snake.pop()
+        snake_set.remove(tail)
+    
     return False
 
-# 메인 실행함수
 def main():
     seconds = 0
-    for d, times in movement:
+    
+    for direction, times in movement:
         for _ in range(times):
             seconds += 1
-            ended = move(d)
-            # print_snake()
-            # print()
-            if ended:
+            if move(direction):
                 print(seconds)
                 return
     
     print(seconds)
-    return
 
-setUp()
 main()
-
