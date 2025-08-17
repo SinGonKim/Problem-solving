e87ae211dc688c27e2e5a2cd480a67dc241fbae0
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-08-17 17:03:21 +0900

    Title: 핀볼게임, Time: 1097ms, Memory: 17MB, Status: Time Limit Exceed - Codetree

---
+++ 250817/핀볼게임/pinball-game.py
@@ -0,0 +1,56 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+dxs = [0, 1, 0, -1]
+dys = [1, 0, -1, 0]
+
+
+def in_range(x,y):
+    if 0 <= x < n and 0 <= y < n:
+        return True
+    else:
+        return False
+
+
+def game_play(x, y, d):
+    tmp = 1
+
+    while in_range(x, y):
+        nx = x + dxs[d]
+        ny = y + dys[d]
+        tmp += 1
+        if not in_range(nx,ny):break
+        if grid[nx][ny] == 1:
+            if d == 0:
+                d = 3
+            elif d == 1:
+                d = 2
+            elif d == 2:
+                d = 1
+            elif d == 3:
+                    d = 0
+        elif grid[nx][ny] == 2:
+            if d == 0:
+                d = 1
+            elif d == 1:
+                d = 0
+            elif d == 2:
+                d = 3
+            elif d == 3:
+                d = 2
+        x, y = nx, ny
+    
+
+    return tmp
+
+
+ans = 0
+for i in range(n):
+    ans = max(ans, game_play(i,0,0))
+    ans = max(ans, game_play(i,n-1,2))
+
+for j in range(n):
+    ans = max(ans, game_play(0,j,1))
+    ans = max(ans, game_play(n-1,j,3))
+print(ans)
