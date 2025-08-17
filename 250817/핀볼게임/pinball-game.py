3d0c44e32c3e3ec0d31a0872353eaa835902b735
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-08-17 17:53:00 +0900

    Title: 핀볼게임, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250817/핀볼게임/pinball-game.py
+++ 250817/핀볼게임/pinball-game.py
@@ -1,39 +1,45 @@
-#완전탐색
-
-#격자의 크기
 n = int(input())
-arr = [list(map(int,input().split())) for _ in range(n)]
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-def in_range(x,y):
-    return x<n and y<n and x>=0 and y>=0
+dxs = [0, 1, 0, -1]
+dys = [1, 0, -1, 0]
 
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
 
-dxs = [-1,1,0,0]
-dys = [0,0,-1,1]
-
-max_time = 0
-
-#핀볼 게임
-#출발점의 위치를 x,y 로 둬보자. 
-def play(x,y,d):
-    time=1
+def game_play(x, y, d):
+    steps = 1
+    
     while in_range(x,y):
-        if arr[x][y]==1:
-            d = 3-d
-        elif arr[x][y]==2:
-            if d<2:
-                d = d+2
-            else:
-                d = d-2
-        x,y = x+dxs[d],y+dys[d]
-        time+=1
+        
+        # 방향 변경 로직
+        if grid[x][y] == 1:
+            d = 3 - d  # 반대 방향
+        elif grid[x][y] == 2:
+            # 원래 코드의 방향 변경 로직 복원
+            if d == 0:
+                d = 1
+            elif d == 1:
+                d = 0
+            elif d == 2:
+                d = 3
+            elif d == 3:
+                d = 2
+                # 다음 위치 계산
+        x = x + dxs[d]
+        y = y + dys[d]
+        steps += 1
+    
+    return steps
 
-    return time
+# 메모이제이션 제거 - 시작점이 모두 다르므로 효과 제한적
+ans = 0
 
+# 원래 코드와 동일한 순서로 시작점 설정
 for i in range(n):
-    max_time = max(max_time,play(n-1,i,0))
-    max_time = max(play(i,0,3),max_time)
-    max_time = max(play(0,i,1),max_time)
-    max_time = max(play(i,n-1,2),max_time)
+    ans = max(ans, game_play(i, 0, 0))      # 왼쪽 가장자리에서 오른쪽으로
+    ans = max(ans, game_play(i, n-1, 2))    # 오른쪽 가장자리에서 왼쪽으로
+    ans = max(ans, game_play(0, i, 1))      # 위쪽 가장자리에서 아래로
+    ans = max(ans, game_play(n-1, i, 3))    # 아래쪽 가장자리에서 위로
 
-print(max_time)
+print(ans)
