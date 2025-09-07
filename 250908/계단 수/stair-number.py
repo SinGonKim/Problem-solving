a310ac58ec04a27189e96260d38a65048389ab1a
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-09-08 00:02:50 +0900

    Title: 계단 수, Time: 64ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250908/계단 수/stair-number.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+# Please write your code here.
+dp = [[0 for _ in range(10)] for _ in range(n)]
+
+for i in range(1,10):
+    dp[0][i] = 1
+
+for i in range(1, n):
+    for j in range(10):
+        if j-1>=0:
+            dp[i][j] += dp[i-1][j-1] 
+        if j+1<10:
+            dp[i][j] += dp[i-1][j+1]
+        dp[i][j]%=10**9 + 7
+
+print(sum(dp[n-1]))
