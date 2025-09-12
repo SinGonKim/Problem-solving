3a79f0ff1c36d8afa11105f062b6fa2c62ca5e49
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-09-12 09:48:14 +0900

    Title: 나이트, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250912/나이트/knight-movements.py
@@ -0,0 +1,37 @@
+
+
+n = int(input())
+r1, c1, r2, c2 = map(int, input().split())
+
+# Please write your code here.
+def is_range(x,y):
+    if 1<=x<=n and 1<=y<=n:
+        return True
+    return False
+
+
+from collections import deque
+def bfs():
+    que = deque()
+    visited = [[-1 for _ in range(n+1)] for _ in range(n+1)]
+
+    visited[r1][c1] = 0
+    que.append((r1,c1,0))
+
+    dxs = [-2,-1,1,2,-2,-1,1,2]
+    dys = [-1,-2,-2,-1,1,2,2,1]
+    while que:
+        r, c, num = que.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx = r + dx; ny = c + dy
+
+            if nx == r2 and ny == c2:
+                return num + 1
+
+            if is_range(nx,ny) and visited[nx][ny] == -1:
+                que.append((nx,ny,num+1))
+                visited[nx][ny] = visited[r][c]+1
+    return -1
+ans = bfs()
+print(ans)
