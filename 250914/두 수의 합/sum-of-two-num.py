300a22fc000e1aafe9034460db7f9fe4322cb023
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-09-14 16:29:26 +0900

    Title: 두 수의 합, Time: 93ms, Memory: 28MB, Status: Passed - Codetree

--- 250914/두 수의 합/sum-of-two-num.py
+++ 250914/두 수의 합/sum-of-two-num.py
@@ -1,20 +1,23 @@
 n, k = map(int, input().split())
-arr = sorted(list(map(int, input().split())))
+arr = list(map(int, input().split()))
 
 # Please write your code here.
+from collections import Counter
+C = Counter(arr)
+arr = sorted(list(set(arr)))
 left = 0
-right = n-1
+right = len(arr)-1
 ans = 0
 while left<right:
     if arr[left] + arr[right] == k:
-        ans += 1
-        if arr[left+1] - arr[left] <= arr[right]- arr[right-1]:
-            left += 1
-        else:
-            right -= 1
-    
+        ans += C[arr[left]]*C[arr[right]]
+        left += 1
     elif arr[left] + arr[right] > k:
         right -= 1
     else:
         left += 1
+
+for key, value in C.items():
+    if 2*key == k:
+        ans += (value*(value-1))//2
 print(ans)
