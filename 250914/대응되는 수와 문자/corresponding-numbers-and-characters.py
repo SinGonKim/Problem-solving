705c679b1e8ef3370d4e246d1bd3c277bcaf7d56
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-09-14 15:19:14 +0900

    Title: 대응되는 수와 문자, Time: 244ms, Memory: 37MB, Status: Passed - Codetree

--- 250914/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 250914/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -5,8 +5,12 @@ words = [""] + [input() for _ in range(n)]
 queries = [input() for _ in range(m)]
 
 # Please write your code here.
+word2idx = dict()
+for idx, word in enumerate(words):
+    word2idx[word] = idx
+
 for q in queries:
     if q.isdigit():
         print(words[int(q)])
     else:
-        print(words.index(q))
+        print(word2idx[q])
