73b1e919c0945898ce1fdc9e0250dddfec250031
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-09-20 00:05:50 +0900

    Title: 미래가 보이는 가위바위보, Time: 172ms, Memory: 34MB, Status: Passed - Codetree

--- 250920/미래가 보이는 가위바위보/rock-paper-scissors-to-see-the-future.py
+++ 250920/미래가 보이는 가위바위보/rock-paper-scissors-to-see-the-future.py
@@ -19,7 +19,7 @@ for i in range(1,N+1):
         dp[i][2] = dp[i-1][2] + 1
 ans = 0
 for i in range(1,N+1):
-    ans = max(ans, dp[i][0]+ dp[N][1]-dp[i][1], dp[N][2] - dp[i][2])
-    ans = max(ans, dp[i][1]+ dp[N][0]-dp[i][0], dp[N][2] - dp[i][2])
-    ans = max(ans, dp[i][2]+ dp[N][1]-dp[i][1], dp[N][0] - dp[i][0])
+    ans = max(ans, dp[i][0]+ dp[N][1]-dp[i][1], dp[i][0]+  dp[N][2] - dp[i][2])
+    ans = max(ans, dp[i][1]+ dp[N][0]-dp[i][0], dp[i][1]+ dp[N][2] - dp[i][2])
+    ans = max(ans, dp[i][2]+ dp[N][1]-dp[i][1], dp[i][2]+ dp[N][0] - dp[i][0])
 print(ans)
