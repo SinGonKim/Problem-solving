dbb501e5ec6aa6a7e5284ce95bee19d8613c058b
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-09-20 12:57:38 +0900

    Title: 가장 짧은 부분합, Time: 63ms, Memory: 23MB, Status: Wrong Answer - Codetree

---
+++ 250920/가장 짧은 부분합/shortest-subtotal.py
@@ -0,0 +1,17 @@
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+import sys
+ans = sys.maxsize
+
+sum_val = 0
+j = 0
+for i in range(n):
+    while j<n and sum_val+arr[j] < s:
+        sum_val += arr[j]
+        j+= 1
+    if j<n and sum_val+arr[j] >=s:
+        ans = min(ans,j-i+1)
+    sum_val -= arr[i]
+print(ans)
