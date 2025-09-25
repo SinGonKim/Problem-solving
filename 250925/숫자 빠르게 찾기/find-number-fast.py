96a3349e440efa0ae65860d0b3e3940296438ce0
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-09-25 23:41:53 +0900

    Title: 숫자 빠르게 찾기, Time: 141ms, Memory: 24MB, Status: Wrong Answer - Codetree

---
+++ 250925/숫자 빠르게 찾기/find-number-fast.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+queries = [int(input()) for _ in range(m)]
+
+# Please write your code here.
+left = 0
+right = n-1
+for querry in queries:
+    while left<=right:
+        mid = (left+right)//2
+        if arr[mid] < querry:
+            left = mid + 1
+        elif arr[mid] > querry:
+            right = mid - 1
+        else:
+            print(mid+1)
+            break
+    else:
+        print(-1)
+    
+    left = mid-1
+    right = n-1
