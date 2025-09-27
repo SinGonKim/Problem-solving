da9adc6e59f3ba3b583f517097b28ed4486ea9f2
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-09-27 20:39:41 +0900

    Title: 신전 탐험하기 2, Time: 92ms, Memory: 18MB, Status: Passed - Codetree

--- 250927/신전 탐험하기 2/explore-temple-2.py
+++ 250927/신전 탐험하기 2/explore-temple-2.py
@@ -6,9 +6,20 @@ for _ in range(n):
     A.append((left,mid,right))
 
 # Please write your code here.
-dp = [[0 for _ in range(3)] for _ in range(n+1)]
+dp = [[[0]*3 for _ in range(3)] for _ in range(n+1)]
 
 for idx, a in enumerate(A,1):
     for i in range(3):
-        dp[idx][i] = max(dp[idx-1][(i+1)%3] + a[i], dp[idx-1][(i+2)%3]+a[i])
-print(max(dp[n]))
+        if idx == 1:
+            dp[idx][i][i] = a[i]
+            continue
+        for j in range(3):
+            dp[idx][i][j] = max(dp[idx-1][(i+1)%3][j] + a[i], dp[idx-1][(i+2)%3][j]+a[i])
+
+answer = 0
+for i in range(3):
+    for j in range(3):
+        if i == j: continue
+        answer = max(answer, dp[n][i][j])
+
+print(answer)
