14f5b4a4ecec2d8e50b3c251d2750c1424ea49ad
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-09-28 19:09:11 +0900

    Title: 원소의 합이 0, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250928/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -0,0 +1,26 @@
+n = int(input())
+num = [list(map(int, input().split())) for _ in range(4)]
+
+dic = {}
+dic2 = {}
+cnt = 0
+
+# Please write your code here.
+
+for i in range(n):
+    for j in range(n):
+        if num[0][i] + num[1][j] not in dic:
+            dic[num[0][i] + num[1][j]] = 1
+        else:
+            dic[num[0][i] + num[1][j]] += 1
+
+        if num[2][i] + num[3][j] not in dic2:
+            dic2[num[2][i] + num[3][j]] = 1
+        else:
+            dic2[num[2][i] + num[3][j]] += 1
+for i in range(n):
+    for j in range(n):
+        diff = -num[0][i] - num[1][j]
+        if diff in dic2:
+            cnt += dic[-diff] * dic2[diff]
+print(cnt) 
