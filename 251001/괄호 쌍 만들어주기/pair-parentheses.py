7e6d9cbcdf6c0c5f4c78001cedbd7ecba8cd7459
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-10-01 18:30:32 +0900

    Title: 괄호 쌍 만들어주기, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 251001/괄호 쌍 만들어주기/pair-parentheses.py
+++ 251001/괄호 쌍 만들어주기/pair-parentheses.py
@@ -2,13 +2,13 @@ A = input()
 
 # Please write your code here.
 n = len(A)
-count = 0
+open_count = 0  # 지금까지 발견한 '(('의 개수
+total_pairs = 0  # 총 쌍의 개수
 
 # 모든 가능한 '((' 위치를 찾기
 for i in range(n - 1):
     if A[i] == '(' and A[i + 1] == '(':
-        # 이 '((' 뒤에 있는 모든 '))'를 찾기
-        for j in range(i + 2, n - 1):
-            if A[j] == ')' and A[j + 1] == ')':
-                count += 1
-print(count)
+        open_count += 1
+    elif A[i] == ')' and A[i + 1] == ')':
+        total_pairs+= open_count
+print(total_pairs)
