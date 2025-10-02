d0f79a9e9b84f9b6815a3a5ef126b399b3fa5f13
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-10-02 23:34:05 +0900

    Title: 트리의 부모 노드, Time: 405ms, Memory: 48MB, Status: Passed - Codetree

--- 251002/트리의 부모 노드/parent-node-of-the-tree.py
+++ 251002/트리의 부모 노드/parent-node-of-the-tree.py
@@ -2,11 +2,24 @@ n = int(input())
 edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
 
 # Please write your code here.
-from collections import defaultdict
-parents = defaultdict(int)
-
+graph = [[] for _ in range(n+1)]
 for edge in edges:
-    parents[edge[1]] = edge[0]
+    graph[edge[0]].append(edge[1])
+    graph[edge[1]].append(edge[0])
+
+visited = [0 for _ in range(n+1)]
+
+parents = [0 for _ in range(n+1)]
+
+def dfs(s):
+    for x in graph[s]:
+        if visited[x] == 1: continue
+        
+        parents[x] = s
+        visited[x] = 1
+        dfs(x)
+
+dfs(1)
 
 for i in range(2,n+1):
     print(parents[i])
