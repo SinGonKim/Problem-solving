e529e6a98dd49e989afcd577aba34be013a04b2a
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-10-03 06:00:10 +0900

    Title: 가장 먼저 나오는 숫자, Time: 143ms, Memory: 33MB, Status: Passed - Codetree

--- 251003/가장 먼저 나오는 숫자/first-appear-number.py
+++ 251003/가장 먼저 나오는 숫자/first-appear-number.py
@@ -5,17 +5,15 @@ query = list(map(int, input().split()))
 for q in query:
     left = 0
     right = n - 1
-    result = -1
+    result = -2
     
+    # 올바른 방법 (최초 위치 찾기)
     while left <= right:
         mid = (left + right) // 2
-        
-        if arr[mid] == q:
-            result = mid + 1  # 1-based index
-            right = mid - 1   # 최초 위치를 찾기 위해 왼쪽 계속 탐색
-        elif arr[mid] < q:
-            left = mid + 1
+        if arr[mid] >= q:  # 같거나 크면
+            if arr[mid] == q:
+                result = mid
+            right = mid - 1  # ✅ 계속 왼쪽 탐색
         else:
-            right = mid - 1
-    
-    print(result)
+            left = mid + 1
+    print(result+1)
