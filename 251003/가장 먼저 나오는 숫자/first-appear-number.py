add6ef85d3bf84e8ea21959523e4a573026a882b
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-10-03 05:57:52 +0900

    Title: 가장 먼저 나오는 숫자, Time: 138ms, Memory: 33MB, Status: Passed - Codetree

--- 251003/가장 먼저 나오는 숫자/first-appear-number.py
+++ 251003/가장 먼저 나오는 숫자/first-appear-number.py
@@ -1,26 +1,21 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-querry = list(map(int, input().split()))
+query = list(map(int, input().split()))
 
-# Please write your code here.
-import bisect
-for q in querry:
+for q in query:
     left = 0
-    right = n-1
+    right = n - 1
+    result = -1
+    
     while left <= right:
-        if q < arr[left] or arr[right]<q:
-            print(-1)
-            break
-        mid = (left+right)//2
-
-        if q == arr[mid]:
-            idx = bisect.bisect_left(arr,mid)
-            print(idx+1)
-            break
+        mid = (left + right) // 2
         
-        elif q > arr[mid]:
+        if arr[mid] == q:
+            result = mid + 1  # 1-based index
+            right = mid - 1   # 최초 위치를 찾기 위해 왼쪽 계속 탐색
+        elif arr[mid] < q:
             left = mid + 1
         else:
             right = mid - 1
-    else:
-        print(-1)
+    
+    print(result)
