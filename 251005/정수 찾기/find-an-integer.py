48699528a4ddfd21e6b553b235146ac57acfbf26
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-10-05 21:20:50 +0900

    Title: 정수 찾기, Time: 116ms, Memory: 29MB, Status: Passed - Codetree

--- 251005/정수 찾기/find-an-integer.py
+++ 251005/정수 찾기/find-an-integer.py
@@ -5,8 +5,9 @@ m = int(input())
 b = list(map(int, input().split()))
 
 # Please write your code here.
-for i in range(m):
-    if b[i] in a:
+set1 = set(a)
+for e in b:
+    if e in set1:
         print(1)
     else:
         print(0)
