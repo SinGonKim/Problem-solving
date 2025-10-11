1d69166dae460aa5a574b495bc2d5a90539cfc5f
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-10-12 00:02:10 +0900

    Title: 각 정점까지의 최단 경로 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251012/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+
+# Please write your code here.
+import sys
+INF = sys.maxsize
+graph = [[INF for _ in range(n+1)] for _ in range(n+1)]
+
+for i in range(n+1):
+    graph[i][i] = 0
+
+for edge in edges:
+    x, y, w = edge
+    graph[x][y] = min(graph[x][y], w)
+
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+for i in range(1,n+1):
+    print(*graph[i][1:])
