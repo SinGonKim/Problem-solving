9350ece0795e1fd6c94b8db42a2eec54f8315e95
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-10-12 00:03:00 +0900

    Title: 각 정점까지의 최단 경로 2, Time: 197ms, Memory: 27MB, Status: Passed - Codetree

--- 251012/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.py
+++ 251012/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.py
@@ -17,5 +17,11 @@ for k in range(1,n+1):
     for i in range(1,n+1):
         for j in range(1,n+1):
             graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
 for i in range(1,n+1):
-    print(*graph[i][1:])
+    for j in range(1,n+1):
+        if graph[i][j] == INF:
+            print(-1, end = ' ')
+        else:
+            print(graph[i][j], end= ' ')
+    print()
