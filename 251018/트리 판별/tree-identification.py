d250a420999b38d09f2ca951f6e902e6d513488b
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-10-18 00:44:17 +0900

    Title: 트리 판별, Time: 54ms, Memory: 17MB, Status: Passed - Codetree

--- 251018/트리 판별/tree-identification.py
+++ 251018/트리 판별/tree-identification.py
@@ -1,46 +1,65 @@
-m = int(input())
-edges = [tuple(map(int, input().split())) for _ in range(m)]
 
-# Please write your code here.
-is_root = [True for _ in range(10001)]
-used = [False for _ in range(10001)]
-root = None
-graph = [[] for _ in range(10001)]
-
-
-for edge in edges:
-    s, e = edge[0], edge[1]
-    is_root[e] = False
-    used[s] = True
-    used[e] = True
-    graph[s].append(e)
-
-for i in range(1,m+1):
-    if used[i] == True and is_root[i] == True:
-        root = i
-        break
-
-from collections import deque
-que = deque()
-que.append(root)
-visited = [0 for _ in range(10001)]
-visited[root] = 1
-
-is_tree = True
-while que:
-    idx = que.popleft()
-    # print(idx)
-    for nx in graph[idx]:
-        if visited[nx]:
-            is_tree = False
+def DFS(x):
+    for y in graph[x]:
+        if visited[y]:
             continue
-        visited[nx] = 1
-        que.append(nx)
+        visited[y]=True
+        DFS(y)
+    
+    return 
 
-for node in range(1,10001):
-    if used[node] == True and visited[node] == 0:
-        is_tree= False
-if is_tree:
-    print(1)
-else:
-    print(0)
+
+if __name__=="__main__":
+    m=int(input())
+    root = 0            # 루트 노드 
+    is_tree = True
+    
+    MAX_N = 10_000      # 최대 노드 개수 
+    graph = [[] for _ in range(MAX_N+1)]
+    visited = [False]*(MAX_N+1)
+    used = [False]*(MAX_N+1)
+    arr = [0]*(MAX_N+1)
+    for _ in range(m):
+        x,y=map(int, input().split())
+        graph[x].append(y)
+        used[x]=True
+        used[y]=True
+        arr[y] += 1 # 진입차수, 노드별 들어오는 간선의 개수 
+        
+
+    
+    # 1. 루트 노드를 찾는다. "들어오는 간선이 하나도 없는 노드"가 여러개 이면 트리가 아니다
+    # for문을 통해 모든 노드들을 확인 
+    for node in range(1, MAX_N+1):
+        if used[node]==True and arr[node]==0:   # node번호가 사용되는데 이 노드로 진입을 안해 
+            if root != 0:
+                is_tree = False 
+
+            root=node
+            
+    # 1.1 루트 노드가 없으면 트리가 아니다 
+    if root == 0:
+        is_tree=False
+        
+
+    # 2. 루트 노드를 제외한 다른 노드들로 들어오는 간선은 전부 1개씩이어야만 한다.
+    for node in range(1,MAX_N+1):
+        # 사용되고 root가 아닌 노드인데 진입차수가 1이 아닌 노드(즉, 진입차수가 2이상)인 거가 있으면 트리 아님 
+        if used[node]==True and node != root and arr[node] != 1:
+            is_tree = False
+    
+    # 좋아 위 조건을 만족해서 일단 Tree라고 치면
+    # 3. root 노드로부터 모든 노드로 갈 수 있는지 판단해야 한다. 
+    if is_tree and root != 0:
+        visited[root]=True      # root 노드를 시작으로  
+        DFS(root)               # 연결된 모든 노드 visited를 True로 바꿔줌 
+    
+    for node in range(1, MAX_N+1):
+        # node 번호가 사용은 되는데 방문한적이 없다? -> 트리 아님 
+        if used[node]==True and visited[node]==False:
+            is_tree = False
+    
+    if is_tree:
+        print(1)
+    else:
+        print(0)
