66235dfea6e77799c8b1911e1efbb77a66f09f7c
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-10-18 14:21:44 +0900

    Title: 최소 차 분할, Time: 63ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 251018/최소 차 분할/minimum-diff-partition.py
+++ 251018/최소 차 분할/minimum-diff-partition.py
@@ -6,19 +6,14 @@ import sys
 INT_MAX = sys.maxsize
 
 s = sum(arr)
-dp = [[0 for _ in range(s+1)] for _ in range(n+1)]
+dp = [0 for _ in range(s+1)]
+dp[0] = 1
 
-dp[0][0] = 1
-
-for i in range(1,n+1):
-    for j in range(s+1):
-        if j >= arr[i-1] and dp[i-1][j-arr[i-1]]:
-            dp[i][j] = 1
-        if dp[i-1][j]:
-            dp[i][j] = 1
-
+for i in range(n):
+    for j in range(s,0,-1):
+        if j >= arr[i] and dp[j-arr[i]]:
+            dp[j] = 1
 ans = INT_MAX
-for i in range(1,s):
-    if dp[n][i]:
-        ans = min(ans, abs(s-2*i))
+for i in range(1,s+1):
+    ans = min(ans, abs(s-2*i))
 print(ans)
