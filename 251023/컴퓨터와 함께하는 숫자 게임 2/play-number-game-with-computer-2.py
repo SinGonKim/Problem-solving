81f98e1568cfdaa2221f6ae28ab61a4daa7693cd
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-10-23 00:53:29 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251023/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
+++ 251023/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
@@ -2,26 +2,35 @@ m = int(input())
 a, b = map(int, input().split())
 
 # Please write your code here.
-C = [0 for _ in range(m+1)]
 
-for i in range(a,b+1):
-    if C[i] == 0:
-        left = 1
-        right = m
-        cnt = 0
-        while left <= right:
-            cnt += 1
-            mid = (left+right)//2
-            if C[mid] == 0:
-                C[mid] = cnt
-            if mid < i:
-                left = mid + 1
-            elif mid >i :
-                right = mid - 1
-            else:
-                break
+import sys
+min_cnt = sys.maxsize
+max_cnt = -sys.maxsize
 
-MIN = min(C[a:b+1])
-MAX = max(C[a:b+1])
-# print(C)
-print(MIN, MAX)
+def binary_search(target_num):
+    left = 1
+    right = m
+    cnt = 0
+
+    while left <= right:
+        cnt += 1
+
+        mid = (left + right)//2
+
+        if mid == target_num:
+            return cnt
+        
+        if mid > target_num:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return cnt
+
+
+while a<= b:
+    target_num = a
+    cnt = binary_search(target_num)
+    max_cnt = max(cnt, max_cnt)
+    min_cnt = min(cnt, min_cnt)
+    a += 1
+print(min_cnt, max_cnt)
