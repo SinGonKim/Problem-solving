3b1fb27262595dbd09ada6390548ee496f48e38f
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-10-30 01:18:17 +0900

    Title: 노드의 인접한 공통 조상, Time: 177ms, Memory: 56MB, Status: Passed - Codetree

--- 251030/노드의 인접한 공통 조상/adjacent-common-ancestor-nodes.py
+++ 251030/노드의 인접한 공통 조상/adjacent-common-ancestor-nodes.py
@@ -1,17 +1,18 @@
+import sys
+sys.setrecursionlimit(10**5+1)
 n = int(input())
 
 graph = [[] for _ in range(n+1)]
-parents = [0 for _ in range(n+1)]
-root = [1 for _ in range(n+1)]
+parents = [i for i in range(n+1)]
+
 for _ in range(n - 1):
     f, t = map(int, input().split())
     graph[f].append(t)
     parents[t] = f
-    root[t] = 0
+
 for i in range(1,n+1):
-    if root[i] == 1:
+    if parents[i] == i:
         root = i
-        break
 
 depth = [0 for _ in range(n+1)]
 def dfs(x):
