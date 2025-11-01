6b2bca372a3e0896ea59b6f32cb9a1c08899dc98
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-11-01 14:03:26 +0900

    Title: 가장 긴 좌우 대칭인 문자열 찾기 3, Time: 114ms, Memory: 37MB, Status: Passed - Codetree

--- 251101/가장 긴 좌우 대칭인 문자열 찾기 3/longest-palindrome-3.py
+++ 251101/가장 긴 좌우 대칭인 문자열 찾기 3/longest-palindrome-3.py
@@ -1,64 +1,65 @@
-def manacher(s):
-    # '#'을 삽입하여 홀수 길이로 변환
-    T = '#'.join('^{}$'.format(s))
-    n = len(T)
-    P = [0] * n
-    C = R = 0
-    
-    for i in range(1, n-1):
-        # 미러 인덱스
-        i_mirror = 2*C - i
-        
-        if R > i:
-            P[i] = min(R - i, P[i_mirror])
-        
-        # 중심에서 확장
-        while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
-            P[i] += 1
-        
-        # 경계 업데이트
-        if i + P[i] > R:
-            C, R = i, i + P[i]
-    
-    return P
-
 def solve():
     line = input().split()
     n = int(line[0])
     exclude_char = line[1]
     S = input()
     
-    # Manacher로 모든 팰린드롬 길이 구하기
-    P = manacher(S)
-    T = '#'.join('^{}$'.format(S))
+    # exclude_char의 모든 위치 찾기
+    positions = [-1]  # 시작 위치
+    for i in range(n):
+        if S[i] == exclude_char:
+            positions.append(i)
+    positions.append(n)  # 끝 위치
     
     max_length = 0
     
-    for i in range(2, len(T)-2):
-        # 팰린드롬 길이
-        length = P[i]
-        center_in_S = (i - 2) // 2
+    # 각 구간에서 가장 긴 팰린드롬 찾기
+    for i in range(len(positions) - 1):
+        start = positions[i] + 1
+        end = positions[i + 1]
         
-        # 원본 문자열에서 팰린드롬 범위 계산
-        if i % 2 == 0:  # '#' 위치 (짝수 길이 팰린드롬)
-            left = center_in_S - length // 2 + 1
-            right = center_in_S + length // 2
-        else:  # 문자 위치 (홀수 길이 팰린드롬)
-            left = center_in_S - length // 2
-            right = center_in_S + length // 2
-        
-        # 각 가능한 팰린드롬 길이에 대해 체크
-        for l in range(length, -1, -1):
-            if i % 2 == 0:
-                actual_left = center_in_S - l // 2 + 1
-                actual_right = center_in_S + l // 2
-            else:
-                actual_left = center_in_S - l // 2
-                actual_right = center_in_S + l // 2
+        if start < end:
+            # 이 구간에는 exclude_char가 없음
+            segment = S[start:end]
+            seg_len = end - start
+
+            input_str = "#" + "#".join(segment) + "#"
+            N = len(input_str)
+            A = [0 for _ in range(N)]
             
-            if actual_left >= 0 and actual_right < n:
-                substring = S[actual_left:actual_right+1]
-                if exclude_char not in substring:
-                    max_length = max(max_length, len(substring))
+            r, p = -1, -1
+
+            for i in range(N):
+                if r < i:
+                    A[i] = 0
+                else:
+                    ii = 2*p - i
+                    A[i] = min(r-i, A[ii])
+                
+                while i - A[i] -1 >=0 and i+A[i] + 1 < N and input_str[i-A[i]-1] == input_str[i+A[i]+1]:
+                    A[i] += 1
+                if i + A[i] > r:
+                    r, p = i + A[i], i
+            X = max(A)
+            max_length = max(max_length, X)
+            
+            
+            # # 이 구간에서 가장 긴 팰린드롬 찾기 (중심 확장법)
+            # for center in range(seg_len):
+            #     # 홀수 길이
+            #     l, r = center, center
+            #     while l >= 0 and r < seg_len and segment[l] == segment[r]:
+            #         max_length = max(max_length, r - l + 1)
+            #         l -= 1
+            #         r += 1
+                
+            #     # 짝수 길이
+            #     l, r = center, center + 1
+            #     while l >= 0 and r < seg_len and segment[l] == segment[r]:
+            #         max_length = max(max_length, r - l + 1)
+            #         l -= 1
+            #         r += 1
     
     print(max_length)
+
+solve()
