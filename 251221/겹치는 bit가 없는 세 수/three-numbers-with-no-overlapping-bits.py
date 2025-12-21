79826e67eefb2a4f8344b573431ea53b15e4d7bb
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2025-12-21 17:31:36 +0900

    Title: 겹치는 bit가 없는 세 수, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251221/겹치는 bit가 없는 세 수/three-numbers-with-no-overlapping-bits.py
+++ 251221/겹치는 bit가 없는 세 수/three-numbers-with-no-overlapping-bits.py
@@ -9,6 +9,6 @@ if len(arr)<3:
 else:
     answer = 0
     for comb in combinations(arr,3):
-        if comb[0]&comb[1]==0 and comb[1]&comb[2]==0 and comb[2]&comb[0]==0:
+        if sum(comb) == (comb[0]|comb[1]|comb[2]):
             answer = max(answer, sum(comb))
     print(answer)
