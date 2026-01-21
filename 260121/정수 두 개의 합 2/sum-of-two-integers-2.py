8637af9bf3bbb4f6086019a1d8cf950f6d5d2a83
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-01-21 14:39:24 +0900

    Title: 정수 두 개의 합 2, Time: 157ms, Memory: 20MB, Status: Passed - Codetree

--- 260121/정수 두 개의 합 2/sum-of-two-integers-2.py
+++ 260121/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -1,18 +1,13 @@
-from bisect import bisect_left
+from bisect import bisect_right
+n, k = map(int, input().split())
+arr = sorted([int(input()) for _ in range(n)])
 
 # Please write your code here.
-def main():
-    ans = 0
-    for left_idx, small_num in enumerate(arr):
-        if small_num >= k: break
-        large_num = k - small_num
-        right_idx = bisect_left(arr, large_num)
-        if right_idx - left_idx <= 0:break
-        ans += right_idx - left_idx
-    return ans
-
-if __name__ == "__main__":
-    n, k = map(int, input().split())
-    arr = sorted([int(input()) for _ in range(n)])
-    ans = main()
-    print(ans)
+ans = 0
+for left_idx, num in enumerate(arr):
+    if num >= k: break
+    res = k - num
+    right_idx = bisect_right(arr, res)
+    if right_idx - left_idx <= 1:break
+    ans += right_idx - left_idx -1
+print(ans)
