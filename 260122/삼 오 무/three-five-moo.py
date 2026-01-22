a55d38c5b94b36f35e610ead3125a6c3966b0b17
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-01-22 19:01:42 +0900

    Title: 삼 오 무, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260122/삼 오 무/three-five-moo.py
+++ 260122/삼 오 무/three-five-moo.py
@@ -9,9 +9,12 @@ while left <= right:
     y = mid//5
     z = mid//15
     if mid - (x + y - z) == n:
+        while mid%3 == 0 or mid % 5 == 0:
+            mid -= 1
         break
     elif mid - (x + y - z) > n:
         right = mid - 1
     else:
         left = mid + 1
+
 print(mid)
