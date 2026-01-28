0e20f4f7e244ef5b630ae5f8103d1128dd489547
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-01-28 18:09:17 +0900

    Title: M개의 구간을 선택하기, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260128/M개의 구간을 선택하기/select-m-sections.py
+++ 260128/M개의 구간을 선택하기/select-m-sections.py
@@ -1,20 +1,20 @@
 import sys
 INF = sys.maxsize
+
 N, M = map(int, input().split())
-numbers = [0] + list(map(int, input().split()))
+numbers = list(map(int, input().split()))
 
 # Please write your code here.
-dp = [[-INF]*(M+1) for _ in range(N+1)]
-dp[0][0] = 0
-
-for i in range(1, N+1):
-    for j in range(max(1, i-1)):
-        for k in range(i,N+1):
-            for l in range(1,M+1):
-                if dp[j][l-1] == -INF: continue
-                dp[k][l] = max(dp[k][l], dp[j][l-1] + sum(numbers[i:k+1]))
-answer = -INF
-for idx in range(1,N+1):
-    if dp[idx][M] > answer:
-        answer = dp[idx][M]
-print(answer)
+# dp[i][j][state]
+# i: 현재 인덱스, j: 구간 개수, state: 0(미포함), 1(포함)
+dp = [[[-INF]*2 for _ in range(M+1)] for _ in range(N+1)]
+dp[0][0][0] = 0
+for i in range(1,N+1):
+    num = numbers[i-1]
+    for j in range(M+1):
+        dp[i][j][0] = max(dp[i][j][0], dp[i-1][j][1])
+        if j == 0: continue #숫자선택안했으므
+        res_extend = dp[i-1][j][1] + num
+        res_new = dp[i-1][j-1][0] + num
+        dp[i][j][1] = max(res_extend, res_new)
+print(max(dp[N][M]))
