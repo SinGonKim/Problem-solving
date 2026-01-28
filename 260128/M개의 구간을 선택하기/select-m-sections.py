abe9e63250fdc6bbd22304da7cdced1be29138c3
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-01-28 17:50:53 +0900

    Title: M개의 구간을 선택하기, Time: 1098ms, Memory: 23MB, Status: Time Limit Exceed - Codetree

--- 260128/M개의 구간을 선택하기/select-m-sections.py
+++ 260128/M개의 구간을 선택하기/select-m-sections.py
@@ -1,15 +1,20 @@
+import sys
+INF = sys.maxsize
 N, M = map(int, input().split())
 numbers = [0] + list(map(int, input().split()))
 
 # Please write your code here.
-dp = [[-1]*(M+1) for _ in range(N+1)]
+dp = [[-INF]*(M+1) for _ in range(N+1)]
 dp[0][0] = 0
 
 for i in range(1, N+1):
     for j in range(max(1, i-1)):
         for k in range(i,N+1):
             for l in range(1,M+1):
-                if dp[j][l-1] == -1: continue
+                if dp[j][l-1] == -INF: continue
                 dp[k][l] = max(dp[k][l], dp[j][l-1] + sum(numbers[i:k+1]))
-
-print(dp[N][M])
+answer = -INF
+for idx in range(1,N+1):
+    if dp[idx][M] > answer:
+        answer = dp[idx][M]
+print(answer)
