532d907fe41bd5813dec07f1fb3dcdba4a784176
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-02 18:24:50 +0900

    Title: 수들의 합 최대화하기, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

--- 260202/수들의 합 최대화하기/max-sum-of-numbers.py
+++ 260202/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -1,16 +1,21 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
-rows = [i for i in range(n)]
-cols = [i for i in range(n)]
+# dp[mask]: mask(비트)에 해당하는 열들을 이미 채웠을 때의 최대값
+dp = [-1] * (1 << n)
+dp[0] = 0
 
-from itertools import permutations
-answer = 0
-for rs in permutations(rows,n):
-    for cs in permutations(cols,n):
-        cnt = 0
-        for r, c in zip(rs, cs):
-            cnt += grid[r][c]
-        answer = max(answer, cnt)
-print(answer)
+for mask in range(1 << n):
+    if dp[mask] == -1: continue
+    
+    # 다음에 채울 행의 번호는 현재 mask에서 1이 된 비트의 개수와 같음
+    r = bin(mask).count('1')
+    if r == n: break
+    
+    for c in range(n):
+        # c번째 열을 아직 사용하지 않았다면
+        if not (mask & (1 << c)):
+            next_mask = mask | (1 << c)
+            dp[next_mask] = max(dp[next_mask], dp[mask] + grid[r][c])
+
+print(dp[(1 << n) - 1])
