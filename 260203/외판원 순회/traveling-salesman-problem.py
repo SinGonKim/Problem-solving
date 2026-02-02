720a73e219e08f9ddfc754001779837605e6e186
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-03 03:11:00 +0900

    Title: 외판원 순회, Time: 942ms, Memory: 25MB, Status: Passed - Codetree

--- 260203/외판원 순회/traveling-salesman-problem.py
+++ 260203/외판원 순회/traveling-salesman-problem.py
@@ -1,26 +1,34 @@
-
+import sys
+INF = sys.maxsize
 
 # Please write your code here.
 def simulate(s, v):
-    global visited, answer
-    visited[s] = 1
+    global visited, answer, order
+
     if sum(visited) == n:
-        answer = max(answer, v)
+        if A[s][0] == 0:
+            return
+        v += A[s][0]
+        if answer > v:
+            answer = v
+            # print(order, answer)
         return
 
-    for i in range(1,n):
-        if visited[i] or not A[s][i]:continue
+    for i in list(set([i for i in range(n)])-set(order)):
+        if visited[i] or A[s][i] == 0:continue
         visited[i] = 1
+        order.append(i)
         simulate(i, v + A[s][i])
         visited[i] = 0
-    
-
+        order.pop()
 
 
 if __name__ == "__main__":
     n = int(input())
     A = [list(map(int, input().split())) for _ in range(n)]
     visited = [0 for _ in range(n)]
-    answer = 0
+    order = [0]
+    answer = INF
+    visited[0] = 1
     simulate(0, 0)
     print(answer)
