4cb893726c76bf69f915fe4a9b5ac37fc35374a2
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-05 14:34:57 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 203ms, Memory: 23MB, Status: Passed - Codetree

--- 260205/수들 중 최솟값 최대화하기/maximin-of-numbers.py
+++ 260205/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -1,22 +1,23 @@
 import sys
 INF = sys.maxsize
+sys.setrecursionlimit(10*6)
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 answer = 0
 # Please write your code here.
 
-def solution(row, prev_cols, cnt):
+def solution(row, visited, cnt):
     global answer
     if row == n:
         answer = max(answer, cnt)
         return
-    
-    res_col = list(set([i for i in range(n)]) - set(prev_cols))
 
-    for col in res_col:
-        prev_cols.append(col)
-        solution(row+1, prev_cols, min(cnt, grid[row][col]))
-        prev_cols.pop()
+    for col in range(n):
+        if visited[col]: continue
+        visited[col] = 1
+        solution(row+1, visited, min(cnt, grid[row][col]))
+        visited[col] = 0
 
-solution(0,[],INF)
+visit = [0 for _ in range(n)]
+solution(0, visit, INF)
 print(answer)
