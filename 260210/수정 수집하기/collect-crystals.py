ff7f715ad66c3e76d48c0f62669c964935571fc8
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-10 19:29:42 +0900

    Title: 수정 수집하기, Time: 62ms, Memory: 17MB, Status: Passed - Codetree

--- 260210/수정 수집하기/collect-crystals.py
+++ 260210/수정 수집하기/collect-crystals.py
@@ -1,37 +1,36 @@
-n, k = map(int, input().split()) # k번 이동해서 최대 수집 수정 개수
-string = 'L' + input()
 
 
-# Please write your code here.
-dp = [[[0]*(k+1) for _ in range(2)] for _ in range(n+1)]
+import sys
 
-def initialize():
-    dp[0][0][k] = 1
+def solve():
+    # 입력 처리
+    n, k = map(int, input().split()) # k번 이동해서 최대 수집 수정 개수
+    s = input()
 
-def calculation(cnt):
-    dir_map = {'L': 0, 'R': 1}
-    for idx in range(1,n+1):
-        s = string[idx]
-        d = dir_map[s]
+    # dp[시간][이동횟수]
+    # dp[i][j]: i번째 시간까지 j번 이동했을 때의 최대 수정 개수
+    dp = [[0] * (k + 1) for _ in range(n + 1)]
 
-        # 움직이지 않을 시
-        if dp[idx-1][d][cnt] != 0:
-            dp[idx][d][cnt] = max(dp[idx][d][cnt], dp[idx-1][d][cnt] + 1)
-        dp[idx][(d+1)%2][cnt] = dp[idx-1][(d+1)%2][cnt]
+    for i in range(1, n + 1):
+        # 현재 떨어지는 수정의 위치 (L=0, R=1)
+        current_pos = 0 if s[i-1] == 'L' else 1
         
-        # 움직일 기회가 있고 움직일 때
-        if cnt >= 1:
-            dp[idx][d][cnt-1] = max(dp[idx][d][cnt-1], dp[idx-1][(d+1)%2][cnt]+1)
-         
-        
-def solution():
-    for num in range(k, -1, -1):
-        calculation(num)
-    
-    ans = max([max(c) for c in dp[n-1]])
-    print(ans)
+        for j in range(k + 1):
+            # j번 이동했을 때 엘라의 현재 위치
+            # 0번 이동: L(0), 1번 이동: R(1), 2번 이동: L(0) ...
+            ella_pos = j % 2
+            
+            # 수정을 얻을 수 있는지 확인
+            score = 1 if ella_pos == current_pos else 0
+            
+            # 1. 이동하지 않고 이전 시간에서 그대로 온 경우
+            dp[i][j] = dp[i-1][j] + score
+            
+            # 2. 이전에 j-1번 이동했다가 지금 1번 더 이동해서 온 경우
+            if j > 0:
+                dp[i][j] = max(dp[i][j], dp[i-1][j-1] + score)
 
+    # 모든 이동 횟수 중 최댓값 출력
+    print(max(dp[n]))
 
-initialize()
-
-solution()
+solve()
