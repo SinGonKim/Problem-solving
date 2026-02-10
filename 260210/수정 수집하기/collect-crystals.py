94d59ba4f3448d4fb6a4f5ff311626d04ed0ea59
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-10 19:17:23 +0900

    Title: 수정 수집하기, Time: 64ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 260210/수정 수집하기/collect-crystals.py
+++ 260210/수정 수집하기/collect-crystals.py
@@ -1,33 +1,36 @@
 n, k = map(int, input().split()) # k번 이동해서 최대 수집 수정 개수
-string = input()
+string = 'L' + input()
 
 
 # Please write your code here.
-dp = [[[0]*(k+1) for _ in range(2)] for _ in range(n)]
+dp = [[[0]*(k+1) for _ in range(2)] for _ in range(n+1)]
 
 def initialize():
-    if string[0] == 'L':
-        dp[0][0][k] = 1
-    else:
-        dp[0][1][k] = 1
+    dp[0][0][k] = 1
 
-def calculation(idx, d):
-    for j in range(k+1):
-        dp[idx][d][j] = max(dp[idx][d][j], dp[idx-1][d][j] + 1)
-        dp[idx][(d+1)%2][j] = dp[idx-1][(d+1)%2][j] 
-    for j in range(k):
-        dp[idx][d][j] = max(dp[idx][d][j], dp[idx-1][(d+1)%2][j+1]+1)
+def calculation(cnt):
+    dir_map = {'L': 0, 'R': 1}
+    for idx in range(1,n+1):
+        s = string[idx]
+        d = dir_map[s]
+
+        # 움직이지 않을 시
+        if dp[idx-1][d][cnt] != 0:
+            dp[idx][d][cnt] = max(dp[idx][d][cnt], dp[idx-1][d][cnt] + 1)
+        dp[idx][(d+1)%2][cnt] = dp[idx-1][(d+1)%2][cnt]
+        
+        # 움직일 기회가 있고 움직일 때
+        if cnt >= 1:
+            dp[idx][d][cnt-1] = max(dp[idx][d][cnt-1], dp[idx-1][(d+1)%2][cnt]+1)
+         
         
 def solution():
-    for i in range(1, n):
-        s = string[i]
-        if s == 'L':
-            calculation(i, 0)
-        else:
-            calculation(i, 1)
+    for num in range(k, -1, -1):
+        calculation(num)
     
     ans = max([max(c) for c in dp[n-1]])
     print(ans)
+
 
 initialize()
 
