4e72f5c31f8f030db29280f0778a42a47f863b4c
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-13 17:07:23 +0900

    Title: 2명의 도둑, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260213/2명의 도둑/two-thieves.py
+++ 260213/2명의 도둑/two-thieves.py
@@ -3,21 +3,50 @@ weight = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 import heapq
+
+def solve_max_square_sum(nums, C):
+    # dp[w]는 원소들의 합이 w일 때 얻을 수 있는 '최대 제곱합'
+    # 초기값 -1은 해당 합계를 만들 수 없음을 의미
+    dp = [-1] * (C + 1)
+    dp[0] = 0
+    
+    # 경로 추적을 위해 (이전 합계, 사용된 원소의 인덱스) 저장
+    parent = [None] * (C + 1)
+
+    for idx, num in enumerate(nums):
+        num_sq = num ** 2
+        # 역순 순회하여 자기 자신 중복 선택 방지
+        for w in range(C, num - 1, -1):
+            if dp[w - num] != -1:
+                # 현재 저장된 값보다 '이 숫자를 추가했을 때의 제곱합'이 더 크면 갱신
+                if dp[w - num] + num_sq > dp[w]:
+                    dp[w] = dp[w - num] + num_sq
+                    parent[w] = (w - num, idx)
+
+    # 전체 dp 테이블에서 가장 큰 제곱합(Value)과 그때의 합계(Weight) 찾기
+    max_sq_val = -1
+    best_w = 0
+    for w in range(C + 1):
+        if dp[w] > max_sq_val:
+            max_sq_val = dp[w]
+            best_w = w
+
+    # 역추적하여 어떤 숫자들이 선택되었는지 리스트업
+    selected_elements = []
+    curr_w = best_w
+    while curr_w > 0 and parent[curr_w] is not None:
+        prev_w, idx = parent[curr_w]
+        selected_elements.append(nums[idx])
+        curr_w = prev_w
+
+    return max_sq_val, best_w, selected_elements
+
 heap = []
 for i in range(n):
     for j in range(n):
-        k = j+1
-        w = weight[i][j]
-        result = w**2
-        while k<n and k - j < m:
-            if w + weight[i][k] <= c:
-                w += weight[i][k]
-                result += weight[i][k]**2
-                k += 1
-            else:
-                k -= 1
-                break
-        heapq.heappush(heap, (-result, i, j, k))
+        candidates = weight[i][j:min(j+m,n)]
+        max_sq, total_sum, elements = solve_max_square_sum(candidates, c)
+        heapq.heappush(heap, (-max_sq, i, j, min(j+m,n)))
 
 ans = 0
 prev_row = -1
