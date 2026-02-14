4839c11d883e98e2a12bdb2a90384f792f88422d
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-14 18:01:05 +0900

    Title: 알파벳과 사칙연산, Time: 90ms, Memory: 20MB, Status: Passed - Codetree

--- 260214/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 260214/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -1,26 +1,44 @@
-expression = input()
-
-# Please write your code here.
-variables = tuple(set(expression[::2]))
-calculations = expression[1::2]
-terms = expression[::2]
-
-cases = []
-
-numbers = [1, 2, 3, 4]*len(variables)
-from itertools import combinations
 import sys
-answer = -sys.maxsize
-for comb in combinations(numbers, len(variables)):
-    Z = dict(zip(variables, comb))
-    prev_term = Z[terms[0]]
-    for idx, cal in enumerate(calculations):
-        if cal == '-':
-            prev_term -= Z[terms[idx+1]]
-        elif cal == '+':
-            prev_term += Z[terms[idx+1]]
-        elif cal == '*':
-            prev_term *= Z[terms[idx+1]]
-    answer = max(answer, prev_term)
-print(answer)
-        
+
+INT_MIN = -sys.maxsize
+
+# 변수 선언 및 입력:
+n = 6
+expression = input()
+num = [0 for _ in range(n)]
+ans = INT_MIN
+
+
+def conv(idx):
+    return num[ord(expression[idx]) - ord('a')]
+
+
+def calc():
+    length = len(expression)
+    value = conv(0)
+    for i in range(2, length, 2):
+        if expression[i - 1] == '+':
+            value += conv(i)
+        elif expression[i - 1] == '-':
+            value -= conv(i)
+        else:
+            value *= conv(i)
+    return value
+
+
+# 'a'부터 'f'까지 순서대로
+# 0부터 5번째 index까지의 값을 
+# 1~4 중에 하나로 채웁니다.
+def find_max(cnt):
+    global ans
+    
+    if cnt == n:
+        ans = max(ans, calc())
+        return
+    
+    for i in range(1, 5):
+        num[cnt] = i
+        find_max(cnt + 1)
+
+find_max(0)
+print(ans)
