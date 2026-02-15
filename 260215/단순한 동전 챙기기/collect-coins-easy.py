f31ab103468afdf26f9eaa5cde404f640e780911
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-15 23:12:46 +0900

    Title: 단순한 동전 챙기기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260215/단순한 동전 챙기기/collect-coins-easy.py
+++ 260215/단순한 동전 챙기기/collect-coins-easy.py
@@ -10,15 +10,16 @@ for i in range(n):
         elif grid[i][j] == 'E':
             end = (i,j)
         elif grid[i][j] != '.':
-            steps.append((i,j))
+            steps.append((int(grid[i][j]), i, j))
 
-from itertools import permutations
+from itertools import combinations
 import sys
 answer = sys.maxsize
-for per in permutations(steps, 3):
+for comb in combinations(steps, 3):
     prev = start
+    comb_sort = sorted(list(comb))
     res = 0
-    for x, y in per:
+    for _, x, y in comb_sort:
         res += abs(prev[0]-x) + abs(prev[1]-y)
         prev = (x,y)
     res += abs(prev[0]-end[0]) + abs(prev[1]-end[1])
