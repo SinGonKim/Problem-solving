3d197aa28e7fbbd90b9a2e50fed490e600e72444
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-16 18:22:16 +0900

    Title: N개의 점 중 M개 고르기, Time: 80ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260216/N개의 점 중 M개 고르기/choose-m-out-of-n-points.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+import sys
+sys.serrecursionlimit(10**6)
+def solution(idx:int, cnt:int, max_distance:int):
+    global answer
+    if idx == n or cnt == m:
+        answer = min(answer, max_distance if max_distance != 0 else sys.maxsize)
+        return
+    
+    solution(idx+1, cnt, max_distance) # idx번째 포인터 패스
+    target = points[idx]
+    if stacks:
+        for stack in stacks:
+            dist = ((target[0]-stack[0])**2 + (target[1]-stack[1])**2)
+            if dist > max_distance:
+                max_distance = dist
+    stacks.append(target)
+    solution(idx+1, cnt+1, max_distance)
+    stacks.pop()
+
+answer = sys.maxsize
+stacks = []
+solution(0, 0, 0)
+print(answer)
