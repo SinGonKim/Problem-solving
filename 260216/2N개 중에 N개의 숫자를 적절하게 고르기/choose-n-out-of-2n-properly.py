ec574158e0a16c03d93a66f0744f5d1c857c0a4e
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-16 18:05:27 +0900

    Title: 2N개 중에 N개의 숫자를 적절하게 고르기, Time: 55ms, Memory: 16MB, Status: Passed - Codetree

--- 260216/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ 260216/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -1,22 +1,42 @@
+import sys
+
+INT_MAX = sys.maxsize
+
+# 변수 선언 및 입력:
+
 n = int(input())
 num = list(map(int, input().split()))
+visited = [False for _ in range(2 * n)]
 
-# Please write your code here.
-import sys
-sys.setrecursionlimit(10**6)
-answer = sys.maxsize
+ans = INT_MAX
 
-def solve(idx, total, left, right):
-    global answer
 
-    if idx == 2*n:
-        answer = min(answer, abs(total))
+def calc():
+    diff = 0
+    for i in range(2 * n):
+        diff = (diff + num[i]) if visited[i] else diff - num[i]
+    
+    return abs(diff)
+
+
+def find_min(idx, cnt):
+    global ans
+    
+    if cnt == n:
+        ans = min(ans, calc())
         return
-    if left < n:
-        solve(idx+1, total+num[idx], left+1, right)
-    if right < n:
-        solve(idx+1, total-num[idx], left, right+1)
+    
+    if idx == 2 * n:
+        return
+    
+    # 현재 숫자를 첫 번째 그룹에 사용한 경우입니다.
+    visited[idx] = True
+    find_min(idx + 1, cnt + 1)
+    visited[idx] = False
+    
+    # 현재 숫자를 두 번째 그룹에 사용한 경우입니다.
+    find_min(idx + 1, cnt)
+    
 
-
-solve(0, 0, 0, 0)
-print(answer)
+find_min(0, 0)
+print(ans)
