9619d9e91e8a4ff879ef665ef3e9e276b4e0a040
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-16 17:05:56 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 260216/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 260216/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -1,24 +1,34 @@
+import sys
+sys.setrecursion.limit(10*6)
+def is_possible(sequence):
+    """
+    현재 수열이 가능한 수열인지 확인합니다.
+    새로 추가된 마지막 문자를 기준으로 인접한 두 부분 수열을 비교합니다.
+    """
+    length = len(sequence)
+    # 비교할 부분 수열의 길이를 1부터 전체 길이의 절반까지 늘려가며 확인
+    for i in range(1, length // 2 + 1):
+        # 인접한 두 부분 수열 추출
+        right = sequence[length - i:]
+        left = sequence[length - 2*i : length - i]
+        
+        if left == right:
+            return False
+    return True
 
-# Please write your code here.
+def solve(current_seq, n):
+    # 목표 길이에 도달하면 해당 수열을 출력하고 프로그램 종료
+    if len(current_seq) == n:
+        print(current_seq)
+        sys.exit(0) # 가장 사전순으로 앞선 것을 찾았으므로 즉시 종료
 
-def solution(num, cnt):
-    global answer
-    if cnt == 0:
-        answer.append(num)
-        return
-    
-    for c in ["4", "5", "6"]:
-        candidate = num + c
-        for l in range(1, len(candidate)//2+1):
-            if candidate[len(candidate)-l:] == candidate[len(candidate)-2*l:len(candidate)-l]:
-                break
-        else:
-            solution(candidate, cnt-1)
-
+    # 사전순으로 앞선 것부터 시도 (4 -> 5 -> 6)
+    for next_digit in ["4", "5", "6"]:
+        candidate = current_seq + next_digit
+        if is_possible(candidate):
+            solve(candidate, n)
     
 
 if __name__ == "__main__":
     n = int(input())
-    answer = []
-    solution("4", n-1)
-    print(answer[0])
+    solve("", n)
