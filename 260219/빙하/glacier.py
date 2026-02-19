8d372e6ee4cc8d2895a6cd2a8f8d8b4cf6b504df
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-19 17:50:57 +0900

    Title: 빙하, Time: 62ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260219/빙하/glacier.py
+++ 260219/빙하/glacier.py
@@ -9,38 +9,47 @@ def is_range(x, y):
     return 0 <= x < n and 0 <= y < m
 
 def can_go(x, y):
-    return is_range(x,y) and a[x][y] == 1 and not visited[x][y]
+    return is_range(x,y) and visited[x][y] == -1
 
 from collections import deque
 que = deque()
-visited = [[False for _ in range(m)] for _ in range(n)]
+visited = [[-1 for _ in range(m)] for _ in range(n)]
 
 for i in range(n):
-    for j in range(m):
-        if a[i][j] == 0:
-            cnt = 0
-            for dx, dy in zip(dxs, dys):
-                ni = i + dx; nj = j + dy
-                if is_range(ni, nj) and a[ni][nj] == 1:
-                    cnt += 1
-            if 1 <= cnt < 4:
-                que.append((i,j,0))
-                visited[i][j] = True
-cur_time = 0
-res = 0
+    if a[i][0] == 0:
+        visited[i][0] = 0
+        que.append((i,0))
+    if a[i][-1] == 0:
+        visited[i][-1] = 0
+        que.append((i,m-1))
+
+for j in range(m):
+    if a[0][j] == 0:
+        visited[0][j] = 0
+        que.append((0,j))
+    if a[-1][j] == 0:
+        visited[-1][j] = 0
+        que.append((n-1,j))
+
 
 while que:
-    x, y, t = que.popleft()
-    if cur_time == t:
-        res += 1
-    else:
-        cur_time += 1
-        res = 1
+    x, y = que.popleft()
     
     for dx, dy in zip(dxs, dys):
         nx = x + dx; ny = y + dy
         if can_go(nx, ny):
-            que.append((nx, ny, t + 1))
-            visited[nx][ny] = True
-print(cur_time, res)
+            if a[nx][ny] == 0:
+                que.append((nx, ny))
+                visited[nx][ny] = visited[x][y]
+            else:
+                que.append((nx, ny))
+                visited[nx][ny] = visited[x][y] + 1
+            
+last_time = max(visited[i][j] for i in range(n) for j in range(m))
 
+cnt = 0
+for i in range(n):
+    for j in range(m):
+        if visited[i][j] == last_time:
+            cnt += 1
+print(last_time, cnt)
