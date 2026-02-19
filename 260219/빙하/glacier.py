08166b0ef8460220b0161a08a39eda6e2f042c7c
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-19 17:52:57 +0900

    Title: 빙하, Time: 96ms, Memory: 19MB, Status: Wrong Answer - Codetree

--- 260219/빙하/glacier.py
+++ 260219/빙하/glacier.py
@@ -50,6 +50,6 @@ last_time = max(visited[i][j] for i in range(n) for j in range(m))
 cnt = 0
 for i in range(n):
     for j in range(m):
-        if visited[i][j] == last_time:
+        if visited[i][j] == last_time and a[i][j] == 1:
             cnt += 1
 print(last_time, cnt)
