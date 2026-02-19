45e81c2bfae5c6d12a75736c79a4407117073634
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-19 18:03:06 +0900

    Title: 빙하, Time: 841ms, Memory: 29MB, Status: Passed - Codetree

--- 260219/빙하/glacier.py
+++ 260219/빙하/glacier.py
@@ -1,55 +1,68 @@
-n, m = map(int, input().split())
-a = [list(map(int, input().split())) for _ in range(n)]
+import sys
+from collections import deque
 
-# Please write your code here.
+# 입력 받기
+n, m = map(int, sys.stdin.readline().split())
+grid = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
 dxs = [-1, 1, 0, 0]
 dys = [0, 0, -1, 1]
 
-def is_range(x, y):
-    return 0 <= x < n and 0 <= y < m
-
-def can_go(x, y):
-    return is_range(x,y) and visited[x][y] == -1
-
-from collections import deque
-que = deque()
-visited = [[-1 for _ in range(m)] for _ in range(n)]
-
-for i in range(n):
-    if a[i][0] == 0:
-        visited[i][0] = 0
-        que.append((i,0))
-    if a[i][-1] == 0:
-        visited[i][-1] = 0
-        que.append((i,m-1))
-
-for j in range(m):
-    if a[0][j] == 0:
-        visited[0][j] = 0
-        que.append((0,j))
-    if a[-1][j] == 0:
-        visited[-1][j] = 0
-        que.append((n-1,j))
-
-
-while que:
-    x, y = que.popleft()
+def get_outside_air():
+    """
+    현재 grid 상태에서 외부 공기(0)인 부분들을 찾아 visited에 표시합니다.
+    (0,0)은 항상 바깥쪽이므로 여기서 시작하는 BFS를 사용합니다.
+    """
+    is_outside = [[False for _ in range(m)] for _ in range(n)]
+    q = deque([(0, 0)])
+    is_outside[0][0] = True
     
-    for dx, dy in zip(dxs, dys):
-        nx = x + dx; ny = y + dy
-        if can_go(nx, ny):
-            if a[nx][ny] == 0:
-                que.append((nx, ny))
-                visited[nx][ny] = visited[x][y]
-            else:
-                que.append((nx, ny))
-                visited[nx][ny] = visited[x][y] + 1
-            
-last_time = max(visited[i][j] for i in range(n) for j in range(m))
+    while q:
+        x, y = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < n and 0 <= ny < m and not is_outside[nx][ny]:
+                # 빙하가 아닌(0인) 곳만 타고 나갑니다.
+                if grid[nx][ny] == 0:
+                    is_outside[nx][ny] = True
+                    q.append((nx, ny))
+    return is_outside
 
-cnt = 0
-for i in range(n):
-    for j in range(m):
-        if visited[i][j] == last_time and a[i][j] == 1:
-            cnt += 1
-print(last_time, cnt)
+time = 0
+last_ice_count = 0
+
+while True:
+    # 1. 현재 남아있는 빙하의 개수를 세어둡니다.
+    current_ice = []
+    for r in range(n):
+        for c in range(m):
+            if grid[r][c] == 1:
+                current_ice.append((r, c))
+    
+    # 빙하가 하나도 없으면 종료
+    if not current_ice:
+        break
+    
+    # 마지막 빙하 개수를 업데이트 (전부 녹기 전의 상태를 보존)
+    last_ice_count = len(current_ice)
+    
+    # 2. 외부 공기 위치 파악
+    is_outside = get_outside_air()
+    
+    # 3. 이번 초에 녹을 빙하 찾기
+    melt_list = []
+    for x, y in current_ice:
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < n and 0 <= ny < m:
+                if is_outside[nx][ny]: # 외부 공기와 접촉했다면
+                    melt_list.append((x, y))
+                    break
+    
+    # 4. 빙하를 녹임 (동시에 녹아야 하므로 한꺼번에 처리)
+    for x, y in melt_list:
+        grid[x][y] = 0
+        
+    time += 1
+
+print(time, last_ice_count)
