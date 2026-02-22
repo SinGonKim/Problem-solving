165b5ac701c543e27a1da2c6a2fe10626ccd6651
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-02-22 18:59:14 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260222/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
@@ -0,0 +1,17 @@
+n, d = map(int, input().split())
+points = sorted([tuple(map(int, input().split())) for _ in range(n)], key = lambda x: (x[1], x[0]))
+
+# Please write your code here.
+left = 0
+right = 1
+import sys
+answer = sys.maxsize
+
+while right < n:
+    if points[right][1] - points[left][1] < d:
+        right += 1
+        continue
+    answer = min(answer, abs(points[right][0] - points[left][0]))
+    left += 1
+    right = left + 1
+print(answer)
