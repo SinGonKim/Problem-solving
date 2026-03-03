ddbf8e7b2ae2e3d6f5893f829737dcd22d21cef8
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-03-03 17:32:52 +0900

    Title: 친한 점, Time: 1388ms, Memory: 58MB, Status: Wrong Answer - Codetree

---
+++ 260303/친한 점/frendly-point.py
@@ -0,0 +1,33 @@
+n, m = map(int, input().split())
+
+# Store points as list of tuples
+points = [tuple(map(int, input().split())) for _ in range(n)]
+
+# Store queries as list of tuples
+queries = [tuple(map(int, input().split())) for _ in range(m)]
+
+# Please write your code here.
+from sortedcontainers import SortedSet
+
+s = SortedSet(points)
+
+for query in queries:
+    idx = s.bisect_left(query)
+    if idx == 0:
+        print(-1, -1)
+    elif idx == n:
+        print(-1, -1)
+    else:
+        x, y = s[idx]
+        if query[0] == x:
+            while idx < n:
+                x, y = s[idx]
+                if query[1] <= y:
+                    print(x, y)
+                    break
+                idx += 1
+            else:
+                print(-1, -1)
+                continue
+        else:
+            print(x, y)
