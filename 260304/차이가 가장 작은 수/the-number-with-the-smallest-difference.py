4573f4e19bc0ac47fb3a52a74f22e78edbe536b8
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-03-04 21:23:51 +0900

    Title: 차이가 가장 작은 수, Time: 529ms, Memory: 33MB, Status: Passed - Codetree

--- 260304/차이가 가장 작은 수/the-number-with-the-smallest-difference.py
+++ 260304/차이가 가장 작은 수/the-number-with-the-smallest-difference.py
@@ -7,7 +7,7 @@ n = len(s)
 import sys
 answer = sys.maxsize
 for i in range(n-1):
-    min_idx = s.bisect_right(s[i]+m)
+    min_idx = s.bisect_left(s[i]+m)
     if min_idx == n:break
     answer =  min(s[min_idx] - s[i], answer)
 
