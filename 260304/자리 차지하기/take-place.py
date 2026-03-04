87d2d4c8c149981286153ac235d6552ec83a6e78
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-03-04 20:03:09 +0900

    Title: 자리 차지하기, Time: 312ms, Memory: 38MB, Status: Passed - Codetree

--- 260304/자리 차지하기/take-place.py
+++ 260304/자리 차지하기/take-place.py
@@ -1,13 +1,19 @@
+import sys
+from sortedcontainers import SortedSet
+
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
 
-# Please write your code here.
-seats = [False for _ in range(m+1)]
+# 비어있는 의자들 1..m
+s = SortedSet(range(1, m + 1))
 
-for ai in a:
-    for s in range(ai, 0, -1):
-        if not seats[s]:
-            seats[s] = True
-            break
-    else:break
-print(sum(seats))
+ans = 0
+for x in a:
+    idx = s.bisect_right(x) - 1
+    if idx < 0:
+        break  # x 이하에 빈 의자 없음 -> 종료
+    seat = s[idx]
+    s.remove(seat)
+    ans += 1
+
+print(ans)
