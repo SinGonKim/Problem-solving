7503d8394860609a03f841855714c44b6f377feb
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-03-05 21:23:43 +0900

    Title: 작지만 큰 숫자, Time: 259ms, Memory: 37MB, Status: Runtime Error - Codetree

--- 260305/작지만 큰 숫자/small-but-big-number.py
+++ 260305/작지만 큰 숫자/small-but-big-number.py
@@ -7,12 +7,13 @@ query = list(map(int, input().split()))
 # Please write your code here.
 for q in query:
     idx = sequence.bisect_left(q)
-    if idx == 0:
-        if sequence[idx] > q:
-            print(-1)
+    if idx >= n:
+        print(-1)
     elif sequence[idx] == q:
         print(sequence[idx])
         sequence.remove(sequence[idx])
+    elif idx <= 0:
+        print(-1)
     else:
         print(sequence[idx-1])
         sequence.remove(sequence[idx-1])
