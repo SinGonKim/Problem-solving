7a69f56d04fbb00e41643c0bea599edac1996c3d
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-03-08 21:15:08 +0900

    Title: 앞에서부터 삭제하기 2, Time: 102ms, Memory: 27MB, Status: Passed - Codetree

--- 260308/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 260308/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -3,27 +3,21 @@ arr = list(map(int, input().split()))
 
 # Please write your code here.
 import heapq
+
 heap = []
-base = 0
-for x in arr[n-1:]:
-    base += x
-    heapq.heappush(heap, x)
-stack = -1
+s = 0
 answer = 0
-for k in range(n-2,-1,-1):
-    if stack == -1:
-        heapq.heappush(heap, arr[k])
-        s = base + arr[k]
-        res = heapq.heappop(heap)
-        s -= res
-        stack = res
-    else:
-        if stack <= arr[k]:
-            heapq.heappush(heap, arr[k])
-            s = base + arr[k]
-        else:
-            heapq.heappush(heap, stack)
-            s = base + stack
-            stack = arr[k]
-    answer = max(answer, s/(n-k-1))
+
+for k in range(n-1,-1,-1):
+    num = arr[k]
+    heapq.heappush(heap, num)
+    s += num
+
+    if len(heap) >= 2 and k > 0:
+        temp_sum = s - heap[0]
+        avg = temp_sum/(len(heap)-1)
+
+        if avg > answer:
+            answer = avg
+
 print(f"{answer:.2f}")
