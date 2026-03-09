99c5ea539b044d8ca41b4f167988f8c76ca9f652
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-03-10 00:10:44 +0900

    Title: 배열 추출 2, Time: 414ms, Memory: 25MB, Status: Wrong Answer - Codetree

---
+++ 260310/배열 추출 2/array-extraction-2.py
@@ -0,0 +1,34 @@
+n = int(input())
+X = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+import heapq
+import sys
+INF = sys.maxsize
+p_heap = []
+m_heap = []
+
+for x in X:
+    if x > 0:
+        heapq.heappush(p_heap, x)
+    elif x < 0:
+        heapq.heappush(m_heap, -x)
+    else:
+        if len(p_heap) == 0 and len(m_heap) == 0:
+            print(0)
+        elif len(m_heap) == 0:
+            p = heapq.heappop(p_heap)
+            print(p)
+        elif len(p_heap) == 0:
+            m = -heapq.heappop(m_heap)
+            print(m)
+        else:
+            p = heapq.heappop(p_heap)
+            m = -heapq.heappop(m_heap)
+            if p < abs(m):
+                print(p)
+                heapq.heappush(m_heap, m)
+            else:
+                print(m)
+                heapq.heappush(p_heap, p)
+
