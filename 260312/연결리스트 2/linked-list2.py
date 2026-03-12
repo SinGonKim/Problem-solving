4bf64796e9b7d58a95a212c3b206ba6ad9102542
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-03-12 16:49:00 +0900

    Title: 연결리스트 2, Time: 1244ms, Memory: 48MB, Status: Passed - Codetree

--- 260312/연결리스트 2/linked-list2.py
+++ 260312/연결리스트 2/linked-list2.py
@@ -1,70 +1,67 @@
 from typing import List
+
 N = int(input())
 Q = int(input())
 
 class Node:
     def __init__(self, data):
         self.data = data
-        self.prev = None
-        self.next = None
+        self.prev = None   # 이전 노드 번호
+        self.next = None   # 다음 노드 번호
 
-
-# Please write your code here.
-
-Nodes: List[Node] = []
-
-for i in range(N):
-    node = Node(i+1)
-    Nodes.append(node)
-        
+Nodes: List[Node] = [Node(i + 1) for i in range(N)]
 
 for _ in range(Q):
     cmd = list(map(int, input().split()))
-    c = cmd.pop(0)
-    if c == 1:
-        idx = cmd[0]
-        node = Nodes[idx-1]
+    c = cmd[0]
 
-        if node.prev is not None and idx > 1:
-            node_prev = Nodes[node.prev-1]
-            node_prev.next = node.next
-        if node.next is not None and idx < N:
-            node_next = Nodes[node.next-1]
-            node_next.prev = node.prev
+    if c == 1:
+        idx = cmd[1]
+        node = Nodes[idx - 1]
+
+        if node.prev is not None:
+            prev_node = Nodes[node.prev - 1]
+            prev_node.next = node.next
+
+        if node.next is not None:
+            next_node = Nodes[node.next - 1]
+            next_node.prev = node.prev
+
         node.prev = None
         node.next = None
-            
+
     elif c == 2:
-        idx, jdx = cmd[0], cmd[1]
-        cnode = Nodes[idx-1]
-        node = Nodes[jdx-1]
-        assert node.prev == None and node.next == None, "Not Single Node!!"
+        idx, jdx = cmd[1], cmd[2]
+        cnode = Nodes[idx - 1]
+        node = Nodes[jdx - 1]
+
+        # j는 단일 노드라고 문제에서 보장
         if cnode.prev is not None:
-            prev_node = Nodes[cnode.prev-1]
+            prev_node = Nodes[cnode.prev - 1]
             prev_node.next = jdx
             node.prev = cnode.prev
-        cnode.prev = jdx
+
         node.next = idx
+        cnode.prev = jdx
 
     elif c == 3:
-        idx, jdx = cmd[0], cmd[1]
-        node = Nodes[jdx-1]
-        assert node.prev == None and node.next == None, "Not Single Node!!"
-        cnode = Nodes[idx-1]
+        idx, jdx = cmd[1], cmd[2]
+        cnode = Nodes[idx - 1]
+        node = Nodes[jdx - 1]
 
         if cnode.next is not None:
-            next_node = Nodes[cnode.next-1]
+            next_node = Nodes[cnode.next - 1]
             next_node.prev = jdx
             node.next = cnode.next
-        cnode.next = jdx
+
         node.prev = idx
+        cnode.next = jdx
+
     elif c == 4:
-        idx = cmd[0]
-        node = Nodes[idx-1]
-        prev = node.prev if node.prev is not None else 0
-        next = node.next if node.next is not None else 0
-        print(prev, next)
-for i in range(N):
-    node = Nodes[i]
-    next = node.next if node.next is not None else 0
-    print(next, end = ' ')
+        idx = cmd[1]
+        node = Nodes[idx - 1]
+        p = node.prev if node.prev is not None else 0
+        n = node.next if node.next is not None else 0
+        print(p, n)
+
+print(*[(node.next if node.next is not None else 0) for node in Nodes])
