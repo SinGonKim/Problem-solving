4bdabdc3c272bf52ab66f3f8f3831721467076ef
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-03-18 19:25:59 +0900

    Title: 구간에 속한 문자의 개수, Time: 2054ms, Memory: 76MB, Status: Time Limit Exceed - Codetree

--- 260318/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
+++ 260318/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
@@ -3,24 +3,26 @@ n, m, k = map(int, input().split())
 # Read grid as list of strings since we need character-by-character access
 grid = [input() for _ in range(n)]
 
-# Read k queries as tuples
-queries = [tuple(map(int, input().split())) for _ in range(k)]
-
 # Please write your code here.
 mapping = {'a': 0, 'b': 1, 'c': 2}
-board = [[[0]*3 for _ in range(m+1)] for _ in range(n+1)]
+board = [[[0]*2 for _ in range(m+1)] for _ in range(n+1)]
 for i in range(n):
     for j in range(m):
         idx = mapping[grid[i][j]]
-        for k in range(3):
-            board[i+1][j+1][k] = board[i+1][j][k]
-        board[i+1][j+1][idx] += 1
+        for c in range(2):
+            board[i+1][j+1][c] = board[i+1][j][c]
+        if idx != 2:
+            board[i+1][j+1][idx] += 1
 
     
-for query in queries:
-    r1, c1, r2, c2 = query
+for _ in range(k):
+    r1, c1, r2, c2 = tuple(map(int, input().split()))
     answer = [0, 0, 0]
     for i in range(r1, r2+1):
-        for k in range(3):
-            answer[k] += board[i][c2][k] - board[i][c1-1][k]
+        tmp = [0, 0]
+        for c in range(2):
+            tmp[c] = board[i][c2][c] - board[i][c1-1][c]
+            answer[c] += tmp[c]
+        answer[2] += c2 - c1 + 1 - sum(tmp[:2])
+        
     print(*answer)
