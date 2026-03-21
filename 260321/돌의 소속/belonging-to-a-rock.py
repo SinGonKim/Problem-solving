940a538a93067327cd7abfe9b35fe0f87848d575
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2026-03-21 14:44:51 +0900

    Title: 돌의 소속, Time: 356ms, Memory: 37MB, Status: Passed - Codetree

--- 260321/돌의 소속/belonging-to-a-rock.py
+++ 260321/돌의 소속/belonging-to-a-rock.py
@@ -3,15 +3,15 @@ arr = [int(input()) for _ in range(N)]
 queries = [tuple(map(int, input().split())) for _ in range(Q)]
 
 # Please write your code here.
-pre_sum = [[0 for _ in range(4)] for _ in range(N+1)]
+pre_sum = [[0 for _ in range(N+1)] for _ in range(4)]
 for i in range(N):
     for j in range(1,4):
-        pre_sum[i+1][j] = pre_sum[i][j]
-    pre_sum[i+1][arr[i]] += 1
+        pre_sum[j][i+1] = pre_sum[j][i]
+    pre_sum[arr[i]][i+1] += 1
 
 for query in queries:
     s, e = query
     for j in range(1, 4):
-        res = pre_sum[e][j] - pre_sum[s-1][j]
+        res = pre_sum[j][e] - pre_sum[j][s-1]
         print(res, end = ' ')
     print()
